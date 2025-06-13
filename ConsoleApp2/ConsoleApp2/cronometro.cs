d5c816e4b7faaca828a3e357440da7d2a5a19415
valentinacolaneri <colanerivalen2006@gmail.com>
2025-06-13 17:41:40 -0300

    Cronometro

---
+++ ConsoleApp2/ConsoleApp2/cronometro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Cronometro
+    {
+        private int minutos;
+        private int segundos;
+
+        public Cronometro()
+        {
+            minutos = 0;
+            segundos = 0;
+        }
+
+        public void Reiniciar()
+        {
+            minutos = 0;
+            segundos = 0;
+        }
+
+        public void IncrementarTiempo()
+        {
+            segundos++;
+
+            if (segundos >= 60)
+            {
+                minutos++;
+                segundos = 0;
+            }
+        }
+
+        public string MostrarTiempo()
+        {
+            return minutos + " minutos, " + segundos + " segundos";
+        }
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EjercicioCronometro.Ejecutar();
+        }
+    }
+
+    public class EjercicioCronometro
+    {
+        public static void Ejecutar()
+        {
+            Cronometro cronometro = new Cronometro();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                cronometro.IncrementarTiempo();
+            }
+
+            Console.WriteLine(cronometro.MostrarTiempo());
+        }
+    }
+}
