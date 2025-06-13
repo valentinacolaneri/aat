f53271b9514bbd96bebb40ce9466644ab56546a7
valentinacolaneri <colanerivalen2006@gmail.com>
2025-06-13 18:46:39 -0300

    Ejercicio Vehiculos terminado
    Clases terminadas y funcionando

---
+++ Bicicletas, camiones y autos/Bicicletas, camiones y autos/Auto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicicletas__camiones_y_autos
+{
+    class Class1
+    {
+    }
+}
---
+++ Bicicletas, camiones y autos/Bicicletas, camiones y autos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bicicletas__camiones_y_autos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
